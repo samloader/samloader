6ff1ac585dd2cd5a4dcf6feeaa5a5bb4b222513b
ightelsentearten <42424827+ightelsentearten@users.noreply.github.com>
2020-10-23 13:30:17 -0400

    Update versionfetch.py

--- samloader/versionfetch.py
+++ samloader/versionfetch.py
@@ -6,13 +6,17 @@
 import xml.etree.ElementTree as ET
 import requests
 
-def getlatestver(model, region):
+
+def get_latest_ver(model: str, region: str):
+    model = model.upper()
+    region = region.upper()
     r = requests.get("https://fota-cloud-dn.ospserver.net/firmware/" + region + "/" + model + "/version.xml")
     r.raise_for_status()
     root = ET.fromstring(r.text)
     vercode = root.find("./firmware/version/latest").text
     if vercode is None:
-        raise Exception("No latest firmware found")
+        raise ValueError("No latest firmware found")
+
     vc = vercode.split("/")
     if len(vc) == 3:
         vc.append(vc[0])
